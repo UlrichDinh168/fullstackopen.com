75b4e4c31c8498778a0db6ae8fa0cc6793eff2d3
UlrichDinh168 <duong0028@gmail.com>
2019-11-17 03:48:03 +0200

    turn database into object

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,15 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const course = "Half Stack application development";
-const part1 = "Fundamentals of React";
-const exercise1 = 10;
-const part2 = "Using props to pass data";
-const exercise2 = 7;
-const part3 = "State of a component";
-const exercise3 = 14;
-const exercises = exercise1 + exercise2 + exercise3;
-
 const Header = props => {
   return (
     <div>
@@ -20,21 +11,15 @@ const Header = props => {
   );
 };
 
-const Content = () => {
+const Content = props => {
   return (
     <div>
-      <Part part = {part1} exercise = {exercise1}/>
-			<Part part = {part2} exercise = {exercise2}/>
-			<Part part = {part3} exercise = {exercise3}/>
+      <p>
+        {props.part} {props.exercises}
+      </p>
     </div>
   );
 };
-
-const Part = props => {
-	return (
-		<p>{props.part} {props.exercise}</p>
-	)
-}
 
 const Total = props => {
   return (
@@ -45,12 +30,27 @@ const Total = props => {
 };
 
 const App = () => {
+  const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Total exercises={exercises} />
+      <Content part={part1.name} exercises={part1.exercises} />
+      <Content part={part2.name} exercises={part2.exercises} />
+      <Content part={part3.name} exercises={part3.exercises} />
+      <Total exercises={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
